bff19ea5802f13afe52d91c9fdbfd8a3918f2f36
Anette11 <krivaltseva@gmail.com>
2023-02-09 13:03:28 +0400

    Create SearchTextField

---
+++ app/src/main/java/com/example/cfttesttask/ui/components/common/SearchTextField.kt
@@ -0,0 +1,47 @@
+package com.example.cfttesttask.ui.components.common
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.cfttesttask.R
+
+@Composable
+fun SearchTextField(
+    value: String,
+    onValueChange: (String) -> Unit
+) = Row(
+    modifier = Modifier.fillMaxWidth(),
+    verticalAlignment = Alignment.CenterVertically
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = { newValue: String -> onValueChange(newValue) },
+        modifier = Modifier.weight(1f),
+        singleLine = true,
+        label = { Text(text = stringResource(id = R.string.search_text_field_bin)) },
+        trailingIcon = {
+            IconButton(onClick = { }) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+    IconButton(onClick = { }) {
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = null
+        )
+    }
+}
